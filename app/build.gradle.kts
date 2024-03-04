c447813d9413ec3ca07be6a5f9144fd2e25d7556
kagg886 <iveour@163.com>
2024-03-04 12:31:02 +0800

    fix: 🐛 修复Android低版本课程表页面崩溃问题

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         }
 
         debug {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
